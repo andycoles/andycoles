f7a176bbfc260ea06fd97781073610d36baa3602
Andy Coles <andy@fusi.io>
2015-11-16 13:45:52 -0800

    removed config from tracking

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: 3000,
-    sessionSecret: 'bb-login-secret',
-    cookieSecret: 'bb-login-secret',
-    defaultPass: 'Taters&Pie!1...'
-    cookieMaxAge: (1000 * 60 * 60 * 24 * 365)
-};
